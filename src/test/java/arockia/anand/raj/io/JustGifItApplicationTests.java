4acdb7082f5b15be3cd97d1679eb8e6aa8b749fc
Anand <50521705+arockiaanandraj@users.noreply.github.com>
2019-11-11 07:45:44 +0530

    Initial Commit

---
+++ src/test/java/arockia/anand/raj/io/JustGifItApplicationTests.java
@@ -0,0 +1,13 @@
+package arockia.anand.raj.io;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustGifItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
